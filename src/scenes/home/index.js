8f4f3dfc74081a019a16c3a0626bad7864082fc5
Seth Choi <schoi@classy.org>
2018-10-21 20:24:53 -0700

    first commit

---
+++ src/scenes/home/index.js
@@ -0,0 +1,5 @@
+const HomePage = () => (
+  <div>HelloWorld2</div>
+)
+
+export default HomePage
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js'
+  }
+}
